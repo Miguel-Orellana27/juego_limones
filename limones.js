d028d59ea3e167862aacac087429e7edea94db3a
Miguel-Orellana27 <orellana_miguel1@icloud.com>
2026-04-23 14:04:36 -0500

    agragar colision

--- limones.js
+++ limones.js
@@ -5,9 +5,10 @@ const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 const ANCHO_LIMON=20;
-const ALTO_LIMON=20;
+const ALTURA_LIMON=20;
 
 let personajeX=canvas.width/2;
+let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE)
 let limonX=canvas.width/2;
 let limonY=5;
 
@@ -24,16 +25,18 @@ function dibujarSuelo(){
 }
 function dibujarPersonaje(){
     ctx.fillStyle="blue";
-    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,personajeY,ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
 
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
+    detectarColision();
 }
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
+    detectarColision();
 }
 function actualizarPantalla(){
     limpiarCanva();
@@ -48,10 +51,19 @@ function limpiarCanva(){
 
 function dibujarLimon(){
     ctx.fillStyle="green";
-    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON);
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTURA_LIMON);
 }
 
 function bajarLimon(){
     limonY = limonY + 10;
     actualizarPantalla();
+}
+
+function detectarColision(){
+    if(limonX+ANCHO_LIMON>personajeX &&  
+        limonX<personajeX+ANCHO_PERSONAJE && 
+        limonY+ALTURA_LIMON>personajeY &&  
+        limonY<personajeY+ALTURA_PERSONAJE){
+        alert("ATRAPADO!!");
+    }
 }
